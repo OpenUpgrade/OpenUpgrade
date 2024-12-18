5faeb54689af707e946feba99fbc47434548f774
Miquel Raïch <miquel.raich@forgeflow.com>
2024-12-18 13:47:43 +0100

    [OU-IMP] crm_iap_enrich: add noupdate changes

--- openupgrade_scripts/scripts/crm_iap_enrich/16.0.1.1/post-migration.py
+++ openupgrade_scripts/scripts/crm_iap_enrich/16.0.1.1/post-migration.py
@@ -3,6 +3,7 @@ from openupgradelib import openupgrade
 
 @openupgrade.migrate()
 def migrate(env, version):
+    openupgrade.load_data(env.cr, "crm_iap_enrich", "16.0.1.1/noupdate_changes.xml")
     openupgrade.delete_records_safely_by_xml_id(
         env,
         [
